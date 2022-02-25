369921017fee1c450deeb1f3e17c96371d57daa5
Bartek O <bartek@fos.nl>
2022-02-25 09:11:59 +0100

    Created a CommunicationEntity
    and filled it until ZipCode

---
+++ src/WarehouseG.Domain/Entities/CommunicationEntity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseG.Domain.Entities
+{
+    public class CommunicationEntity
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public CommunicationEntity()
+        {
+            // uhhh...
+        }
+
+        /// <summary>
+        /// Id of communication
+        /// </summary>
+        public int Id { get; set; }
+
+
+        /// <summary>
+        /// Street of house/building
+        /// </summary>
+        public string Street { get; set; }
+
+
+        /// <summary>
+        /// Street number of house/building
+        /// </summary>
+        public int Number { get; set; }
+
+
+        /// <summary>
+        /// Street number extension of house/building
+        /// <br />
+        /// Nullable for communications with no extension
+        /// </summary>
+        public string? NumberExtension { get; set; }
+
+
+        /// <summary>
+        /// Zipcode of house/building
+        /// </summary>
+        public string ZipCode { get; set; }
+    }
+}
