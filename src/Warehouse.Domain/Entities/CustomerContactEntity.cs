386aafc1f51e4e2b4dc3cab310ce84cdd6c731a9
Bartek O <bartek@fos.nl>
2022-03-25 10:50:50 +0100

    Created connection table entity for Customer-Contact

---
+++ src/Warehouse.Domain/Entities/CustomerContactEntity.cs
@@ -0,0 +1,30 @@
+﻿namespace Warehouse.Domain.Entities
+{
+    /// <summary>
+    /// Entity for the connection table between the Customer and Contact
+    /// </summary>
+    public class CustomerContactEntity
+    {
+        public CustomerContactEntity() { }
+
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Foreign Key
+        /// </summary>
+        public int CustomerId { get; set; }
+
+        /// <summary>
+        /// Foreign Key
+        /// </summary>
+        public int ContactId { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+
+        public byte[] Timestamp { get; set; }
+
+        public ContactEntity Contact { get; set; }
+    }
+}
